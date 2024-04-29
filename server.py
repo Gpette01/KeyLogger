44f77a29a08ae99250c7fb5cfffa089e283de9f1
Gpette01 <103171617+Gpette01@users.noreply.github.com>
2024-04-29 16:47:25 +0300

    Update server.py
    Interupt now is catched correctly

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import socket
 import sys
+import socket
 
 def server_program():
     # get the hostname
@@ -12,6 +12,9 @@ def server_program():
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
     server_socket.settimeout(1) # timeout for accept function
+
+    conn = None
+    
     try:
         while True:
             try:
@@ -19,21 +22,25 @@ def server_program():
                 print("Connection from: " + str(address))
                 with open("log.txt", "a") as fi:  # create/open and append to log.txt
                     fi.write("Connection from: " + str(address) + "\n")
-            except socket.timeout:  # catch the timeout exception
-                continue
-            while True:
-                # receive data stream. it won't accept data packet greater than 1024 bytes
-                try:
-                    data = conn.recv(1024).decode()
-                    if not data:
-                        # if data is not received break
+                while True:
+                    # receive data stream. it won't accept data packet greater than 1024 bytes
+                    try:
+                        data = conn.recv(1024).decode()
+                        if not data:
+                            # if data is not received break
+                            break
+                        with open("log.txt", "a") as fi:  # use the with statement
+                            fi.write(str(data)) # write to file data receieved
+                    except ConnectionResetError:
+                        print("Client disconnected abruptly")
                         break
-                    with open("log.txt", "a") as fi:  # use the with statement
-                        fi.write(str(data)) # write to file data receieved
-                except ConnectionResetError:
-                    print("Client disconnected abruptly")
-                    break
-            conn.close()  # close the connection
+                conn.close()  # close the connection
+            except socket.timeout:  # catch the timeout exception
+                pass
+            except KeyboardInterrupt: # catch the interupt to properly close server
+                print("\nServer is shutting down")
+                server_socket.close()  # close the server socket
+                sys.exit(0)
     except KeyboardInterrupt: # catch the interupt to properly close server
         print("\nServer is shutting down")
         server_socket.close()  # close the server socket
